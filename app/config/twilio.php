7f1b1a66cd4bf20dfd6f29654fc731611711b3ca
primalbase <kawai@primalbase.jp>
2014-05-04 12:47:25 +0900

    設定ファイルのひな形を追加

---
+++ app/config/twilio.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+
+  'baseUrl'     => 'http://xxxx',
+  'phoneNumber' => 'xxx-xxxx-xxx',
+  'callerId'    => '+xxxxxxxx',
+  'accountSid'  => 'xxxxxxxx',
+  'authToken'   => 'xxxxxxxx',
+
+);
